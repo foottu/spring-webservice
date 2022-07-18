2d376fd276c6056184dca016732d3f53840bd095
foottu <foottumy@gmail.com>
2022-07-19 00:14:00 +0900

    Changes

--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -1,13 +1,24 @@
 package com.jojoldu.webservice.web;
 
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
 
+    private PostsRepository postsRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return  "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
